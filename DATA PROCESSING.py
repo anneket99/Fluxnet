44a21e2c834fb71ab38cdbf0bd447e7b4ec10145
Anneke Tombrägel <110912884+anneket99@users.noreply.github.com>
2022-11-18 16:42:57 +0100

    Update DATA PROCESSING.py
    added new definition location

--- DATA PROCESSING.py
+++ DATA PROCESSING.py
@@ -75,11 +75,54 @@ def select_data(file,time_resolution, Index, Index_Names, path,path_goal):
     data = df[Index]
     data.columns = Index_Names
     file_goal = path_goal+file[:-4]+"_reduced.csv"
-    data.to_csv(file_goal)
+    data.to_csv(file_goal, index = False)
     
 
-
-# In[ ]:
+def location(file,path,path_goal):
+    
+    ''' Input: file ( with information on location), path (with fluxnet files), path_goal (path for storing data)
+    
+    
+    The code first reads in the location and SITE_ID of the input file and then looks for the fluxnet sites with the same SITE_ID. 
+    The SITE_ID and loccation get added to the dataset. All datasets in the given dataset get combined into one dataset.'''
+    
+    result = []
+    
+    # read file with locations
+    with open(file) as f:
+        f = open(file,"r")
+        for x in f:
+            result.append(x.split(';')[1])
+            
+    SITE_ID_ = [x.split(';')[0] for x in open(file).readlines()]
+    SITE_ID = []
+    for i in range (1,len(SITE_ID_)):
+        SITE_ID.append(SITE_ID_[i][1:])
+    
+    #get latitute and longitude
+    LOCATION_LAT = [x.split(';')[4] for x in open(file).readlines()][1:]
+    LOCATION_LONG = [x.split(';')[5] for x in open(file).readlines()][1:]
+    
+    #look for files with same SITE_ID 
+    names = os.listdir(path)
+    data = []
+    for j in range(len(names)):
+        if (names[j].find('reduced') != -1):
+            file = path+names[j]
+            data_ = pd.read_csv(file)
+        
+            for i in range(len(SITE_ID)):
+                
+                # add SITE_ID, Latitude and Longitude
+                if(file.find(SITE_ID[i]) != -1):
+                    data_.insert(1,"SITE_ID",SITE_ID[i])
+                    data_.insert(2,"LAT",LOCATION_LAT[i])
+                    data_.insert(3,"LONG", LOCATION_LONG[i])
+                    try:
+                        data = data.append(data_,ignore_index = True)
+                    except:
+                        data = data_
+    data.to_csv(path_goal, Index = False)
 
 
 
